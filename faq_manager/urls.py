e943deeb04b4298ff56493bc43c37a126b6ce85c
zephyCode <utkarsh.pundeer2001@outlook.com>
2025-02-02 00:34:44 +0530

    urls.py is modified

--- faq_manager/urls.py
+++ faq_manager/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import FAQViewSet
-
-router = DefaultRouter()
-router.register(r'faqs', FAQViewSet)
+from django.contrib import admin
+from django.urls import path
+from .views import faq_list 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('faq/', faq_list, name='faq-list'),
 ]
