ddc61997dd89701be8a3bdd4fa7d300d640b7640
zephyCode <utkarsh.pundeer2001@outlook.com>
2025-02-02 00:24:43 +0530

    views.py and serializers.py file created and urls.py file modified

---
+++ faq_manager/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import FAQ
+from .serializers import FAQSerializer
+
+class FAQViewSet(viewsets.ModelViewSet):
+    queryset = FAQ.objects.all()
+    serializer_class = FAQSerializer
