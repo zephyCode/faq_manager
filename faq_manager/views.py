6f39c557c5390841ecff0025047baede12ee8c71
zephyCode <utkarsh.pundeer2001@outlook.com>
2025-02-02 11:59:38 +0530

    views.py file is modified and issues are fixed for robustly handling the requests

--- faq_manager/views.py
+++ faq_manager/views.py
@@ -1,7 +1,19 @@
-from rest_framework import viewsets
-from .models import FAQ
-from .serializers import FAQSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import redirect
+from .models import Faq
+from .serializers import FaqSerializer
 
-class FAQViewSet(viewsets.ModelViewSet):
-    queryset = FAQ.objects.all()
-    serializer_class = FAQSerializer
+@api_view(['GET', 'POST'])
+def faq_list(request):
+    if request.method == 'GET':
+        faqs = Faq.objects.all()
+        serializer = FaqSerializer(faqs, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = FaqSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return redirect('faq_list')
+        return Response(serializer.errors, status=400)
